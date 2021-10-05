c138d8cf50f25264b0068c26bc465ed239b0a2de
Lee <lee45@csk063.cluster>
2021-10-05 12:36:16 +0200

    updated readme and add NME and NMB calculation into stat.py (based on HydroErr.py)

--- README.md
+++ README.md
@@ -3,19 +3,26 @@
 
 The model predicts air quality prediction based on Dense Neural Network, with varying # of layers. It compares the DNN-based prediction against the random forest-based and CMAQ predictions.  It uses the AQS observation data from the Pacific Northwest, U.S.
 
+
+
 The below is how to execute our model:
 
-```
+```python
 python main.py --flag2rf 0 --flagdensexl 0 --flagdensel 0 --flagdensem 0 --flagdenses 0 --flagdensexs 0 --flagnasmlp 1 --flagnasres 1
 ```
 
 For example:
 
-```
+```python
 python main.py --wrf_path put_WRF_data_path  --aqs_path put_AQS_data_path  --out_path put_output_path
 ```
 
 Important note that WRF and AQS data are pre-processed data, not the original/raw data format. 
+
+
+
+### Contributors: 
+Drs. Zongru (Doris) Shao, Kai Fan, and Yunha Lee at CASUS, HZDR in Germany. 
 
 
 
--- forecast/stat.py
+++ forecast/stat.py
@@ -1,10 +1,39 @@
 import pandas
 
-
-
-
-
 def value_percentage(df, col, value):
     ns, nc = df.shape
     nvs, nc = df.loc[df[col] == value].shape
     return ns, nvs / float(ns)
+
+
+
+from __future__ import division
+import numpy as np
+from scipy.stats import gmean, rankdata
+import warnings
+
+
+def nme(simulated_array, observed_array, replace_nan=None, replace_inf=None,
+       remove_neg=False, remove_zero=False):
+
+    # Treating missing values
+    simulated_array, observed_array = treat_values(simulated_array, observed_array,
+                                                   replace_nan=replace_nan,
+                                                   replace_inf=replace_inf,
+                                                   remove_neg=remove_neg,
+                                                   remove_zero=remove_zero)
+    a = np.abs(simulated_array - observed_array)
+    return 100 * np.sum(a) /np.sum(observed_array)
+
+
+def nmb(simulated_array, observed_array, replace_nan=None, replace_inf=None,
+       remove_neg=False, remove_zero=False):
+
+    # Treating missing values
+    simulated_array, observed_array = treat_values(simulated_array, observed_array,
+                                                   replace_nan=replace_nan,
+                                                   replace_inf=replace_inf,
+                                                   remove_neg=remove_neg,
+                                                   remove_zero=remove_zero)
+    a = np.sum(simulated_array - observed_array)
+    return 100 * a / np.sum(observed_array)
